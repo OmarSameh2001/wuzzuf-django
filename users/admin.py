c9819a394f139203a9a575849d10c8f398dc0d5c
hebatullahgassem <hebatullahgassem@gmail.com>
2025-03-17 16:21:29 +0200

    admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,50 @@
+"""
+django admin customization
+"""
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserAdmin(BaseUserAdmin):
+    #define admin pages for users
+    ordering = ['id']
+    list_display = ['email', 'name', 'username']
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'is_company'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'username',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'is_company'
+            )
+        }),
+    )
+
 
 # Register your models here.
+admin.site.register(models.User, UserAdmin)
