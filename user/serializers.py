19ace66f6619041b350a802de9884c132bd088e2
hebatullahgassem <hebatullahgassem@gmail.com>
2025-04-11 09:50:46 +0200

    password

--- user/serializers.py
+++ user/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'username', 'password', 'name', 'user_type']
 
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
     
     def create(self, validated_data):
         user_type = validated_data.pop("user_type", "jobseeker")  # Default to jobseeker
